5d7283e9c303f07801c52498710ff400ce895784
flickz <omoyajowo2015@gmail.com>
2019-01-18 18:44:46 +0100

    Inital Commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require('graphql-yoga')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const Link = require('./resolvers/Link')
+const User = require('./resolvers/User')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link,
+  Subscription,
+  Vote
+}
+
+const server = new GraphQLServer({
+  typeDefs:'./src/schema.graphql',
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma
+    }
+  }
+})
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
---
+++ src/util.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const APP_SECRET = 'GraphQL-is-awesome'
+
+function getUserId(context) {
+  const Authorization = context.request.get('Authorization')
+  if (Authorization) {
+    const token = Authorization.replace('Bearer', '')
+
+    const { userId } = jwt.verify(token, APP_SECRET)
+    console.log(userId)
+    return userId 
+  }
+
+  throw new Error('Not authenticated')
+}
+
+module.exports = {
+  APP_SECRET,
+  getUserId
+}
